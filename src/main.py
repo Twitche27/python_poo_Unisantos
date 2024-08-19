e1a74e30d2e522399af9d874915f038d119bbdda
Mona Lisa <mateusb.a27@gmail.com>
2024-08-19 10:07:38 -0300

    Simulação de dois carros

--- src/main.py
+++ src/main.py
@@ -1,44 +1,60 @@
 from frota import *
 
+def simular_carro(carro: Carro):
+    print('1- Ligar motor')
+    print('2- Desligar motor')
+    print('3- Acelerar')
+
+    op = 0
+    while op not in (1,2,3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+    if op == 1:
+        carro.ligar()
+    elif op == 2:
+        carro.desligar()
+    elif op == 3:
+        v = float(input("Informe a velocidade: "))
+        t = float(input("Informe o tempo: "))
+        carro.acelerar(v, t)
+
+
 if __name__ == "__main__":
     print('Cadastre um carro')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
 
-    kms = float(input('Digite com quantos Kms: '))
+    #kms = float(input('Digite com quantos Kms: '))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False)
 
+    print('Cadastre outro carro')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False)
     '''
-    Controlando o carro até ele atingir 10000 Km
+    Controlando 2 carros até atingir 300km
     '''
-    while carro1.odometro < 10000:
+    while carro1.odometro < 300 and carro2.odometro < 300:
         try:
-            print('1- Ligar motor')
-            print('2- Desligar motor')
-            print('3- Acelerar')
-
-            op = 0
-            while op not in (1,2,3):
-                op = int(input("Digite as opcoes[1-3]: "))
-
-            if op == 1:
-                carro1.ligar()
-            elif op == 2:
-                carro1.desligar()
-            elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
-
-            print('Infos atuais do carro')
-            print(carro1)
+            op_carro = 0
+            while op_carro not in (1,2):
+                op_carro = int(input('Qual carro você quer manipular 1 ou 2?'))
+            if op_carro == 1:
+                simular_carro(carro1)
+            else:
+                simular_carro(carro2)
         except Exception as e:
             print("Erro!")
             print(e)
-
-    carro1.desligar()
+    try:
+        carro1.desligar()
+        carro2.desligar()
+    except Exception as e:
+        print("Erro!")
+        print(e)
     print(carro1)
-    print('Parar para trocar óleo!!!')
-
+    print(carro2)
