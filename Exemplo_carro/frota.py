4abf88e474c2d14ddcda68fa39f11bdd9d991234
Mona Lisa <dfgag>
2024-08-26 08:37:21 -0300

    Simulação com Gasolina

--- Exemplo_carro/frota.py
+++ Exemplo_carro/frota.py
@@ -2,9 +2,9 @@ class Carro:
     modelo : str
     marca : str
     cor : str
-    odometro : 0.0
-    motor_on : False
-    tanque: float
+    __odometro : 0.0
+    __motor_on : False
+    __tanque: float
     consumo_medio: float
 
     def __init__(self, modelo : str, marca : str, cor : str,
@@ -13,44 +13,50 @@ class Carro:
         self.modelo = modelo
         self.marca = marca
         self.cor = cor
-        self.odometro = odometro
-        self.motor_on = motor
-        self.tanque = tanque
+        self.__odometro = odometro
+        self.__motor_on = motor
+        self.__tanque = tanque
         self.consumo_medio = consumo_medio
 
+    def get_odometro(self):
+        return self.__odometro
+
+    def get_tanque(self):
+        return self.__tanque
+
     def ligar(self):
-        if self.tanque > 0:
-            if not self.motor_on:
-                self.motor_on = True
+        if self.__tanque > 0:
+            if not self.__motor_on:
+                self.__motor_on = True
             else:
                 raise Exception("Erro: Motor já ligado!")
         else:
             raise Exception("Erro: Tanque Vazio")
 
     def acelerar(self, velocidade : float, tempo : float):
-        if self.motor_on:
+        if self.__motor_on:
             kilometragem = velocidade * tempo
             litros = kilometragem*self.consumo_medio
-            if litros <= self.tanque:
-                self.tanque -= litros
-                self.odometro += kilometragem
+            if litros <= self.__tanque:
+                self.__tanque -= litros
+                self.__odometro += kilometragem
             else:
-                self.odometro += kilometragem*(self.tanque/litros)
-                self.tanque = 0
-                self.motor_on = False
+                self.__odometro += kilometragem*(self.__tanque/litros)
+                self.__tanque = 0
+                self.__motor_on = False
         else:
             raise Exception("Erro: Não é possível acelerar! Motor desligado!")
 
     def desligar(self):
-        if self.motor_on:
-            self.motor_on = False
+        if self.__motor_on:
+            self.__motor_on = False
         else:
             raise Exception("Erro: Motor já desligado!")
 
     def __str__(self):
         info = (f'Carro {self.modelo}, marca {self.marca}, '
-                f'cor {self.cor}\n{self.odometro} Km, '
-                f'motor {self.motor_on}, tanque {self.tanque}\n'
+                f'cor {self.cor}\n{self.__odometro} Km, '
+                f'motor {self.__motor_on}, tanque {self.__tanque}\n'
                 f'km/L {self.consumo_medio}')
         return info
 
--- Exemplo_carro/main.py
+++ Exemplo_carro/main.py
@@ -42,8 +42,8 @@ if __name__ == "__main__":
     '''
     Controlando 2 carros até atingir 300km
     '''
-    while ((carro1.odometro < 300 and carro2.odometro < 300) and 
-           (carro1.tanque > 0 or carro2.tanque > 0)):
+    while ((carro1.get_odometro() < 300 and carro2.get_odometro() < 300) and
+           (carro1.get_tanque() > 0 or carro2.get_tanque() > 0)):
         try:
             op_carro = 0
             while op_carro not in (1,2):
