26dc06dd05f36c3b078b1d61ffffc7509f759182
Mateus <@>
2024-10-21 09:55:03 -0300

    .

--- Lutadores.py
+++ Lutadores.py
@@ -9,25 +9,32 @@ class Lutador(ABC):
         self.energia = 100
 
     def soco(self, oponente):
+        print("Soco!")
         oponente.energia -= 5.5
 
     def __str__(self) -> str:
         return f'Nome: {self.nome}\nEnergia: {self.energia}%'
+    
 class Boxeador(Lutador):
     def cruzado(self, oponente:Lutador):
+        print("Cruzado!")
         oponente.energia -= 10.2
 
     def gancho(self, oponente:Lutador):
+        print("Gancho!")
         oponente.energia -= 20.8
 
 class Muay_Thai(Boxeador):
     def chute_alto(self, oponente:Lutador):
+        print("Chute Alto!")
         oponente.energia -= 15.4
 
 class Jiujitsu(Lutador):
     def chave_braco(self, oponente:Lutador):
+        print("Chave de Braço!")
         oponente.energia -= 100
 
 class MMA(Muay_Thai, Jiujitsu):
     def superman_punch(self, oponente:Lutador):
+        print("Superman Punch!")
         oponente.energia -= 53.2
--- main_Lutadores.py
+++ main_Lutadores.py
@@ -1,11 +1,20 @@
+import random
 from Lutadores import *
 
 if __name__ == "__main__":
     jj = Jiujitsu("Lutador 1")
+    jj_ataques = ["soco", "chave_braco"]
     mma = MMA("Lutador 2")
-    jj.soco(mma)
-    print(mma)
-    mma.cruzado(jj)
-    print(jj)
-    mma.superman_punch(jj)
-    print(jj)
+    mma_ataques = ["soco", "cruzado", "gancho", "chute_alto", "chave_braco", "superman_punch"]
+    while (jj.energia > 0 and mma.energia > 0):
+        eval(f'jj.{random.choice(jj_ataques)}(mma)')
+        print(mma)
+        if mma.energia <= 0:
+            break
+        eval(f'mma.{random.choice(mma_ataques)}(jj)')
+        print(jj)
+    if jj.energia <= 0:
+        print(jj.nome + " Perdeu")
+    else:
+        print(mma.nome + " Perdeu")
+
