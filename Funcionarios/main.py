15f603912d5979a953eff970fe552475c349bcb2
Mateus <@>
2024-10-21 08:25:00 -0300

    .

--- Funcionarios/main.py
+++
@@ -1,7 +0,0 @@
-from funcs import *
-
-if __name__ == "__main__":
-    f2 = Gerente("Geraldo", "111111", 123.5, 123, 6661)
-    f3 = OperadorCaixa("Geraldo", "111111", 123.5, 123, 6661)
-    f4 = Seguranca("Geraldo", "111111", 123.5, 123, 6661)
-    f3.autenticar(6661, 123)
